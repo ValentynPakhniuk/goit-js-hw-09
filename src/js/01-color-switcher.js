72a27ee2566c875ff62936caaddecc258f348803
Valentyn Pakhniuk <valentin.pakhniuk@gmail.com>
2023-02-08 16:30:01 +0200

    task 01 color switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerColor = null;
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  timerColor = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick() {
+  clearInterval(timerColor);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
